590bc988be961f70692fa9322d34f7eb4c7c299d
jmoussalli <jerome.moussalli@gmail.com>
2024-01-31 13:24:02 +0100

    Ajout entity Document, tables documentss

--- src/main/java/fr/moussalli/tpmediatheque/domain/Document.java
+++ src/main/java/fr/moussalli/tpmediatheque/domain/Document.java
@@ -1,4 +1,23 @@
 package fr.moussalli.tpmediatheque.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name="documents")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
 public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String nom;
+    private String description;
+
 }
