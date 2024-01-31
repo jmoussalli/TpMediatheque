cf3666df9c192df70a71e79b2dcc79ea2c991e0d
jmoussalli <jerome.moussalli@gmail.com>
2024-01-31 13:32:42 +0100

    Ajout EmpruntRepository

---
+++ src/main/java/fr/moussalli/tpmediatheque/repository/EmpruntRepository.java
@@ -0,0 +1,11 @@
+package fr.moussalli.tpmediatheque.repository;
+
+import fr.moussalli.tpmediatheque.domain.Document;
+import fr.moussalli.tpmediatheque.domain.Emprunt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpruntRepository extends JpaRepository<Emprunt, Integer> {
+
+}
