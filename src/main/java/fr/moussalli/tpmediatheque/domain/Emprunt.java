fbc834bd985a4969dd73907f6d96629ee7023fed
jmoussalli <jerome.moussalli@gmail.com>
2024-01-31 13:26:29 +0100

    Ajout entity Emprunt, tables emprunts

--- src/main/java/fr/moussalli/tpmediatheque/domain/Emprunt.java
+++ src/main/java/fr/moussalli/tpmediatheque/domain/Emprunt.java
@@ -1,4 +1,27 @@
 package fr.moussalli.tpmediatheque.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "emprunts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
 public class Emprunt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "adherent_id")
+    private Adherent adherent;
+    @ManyToOne
+    @JoinColumn(name = "document_id")
+    private Document document;
+
 }
